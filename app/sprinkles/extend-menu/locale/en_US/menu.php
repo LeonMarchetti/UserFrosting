6d5fadf71421f647b1e9853acd7d2a01f245ddba
LeonMarchetti <mleonardoa@gmail.com>
2019-10-08 11:19:57 -0300

    extend-menu: Sprinkle dedicado a los ítems del menú lateral

---
+++ app/sprinkles/extend-menu/locale/en_US/menu.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    "PASTRIES" => [
+        "@TRANSLATION" => "Pastries",
+        "LIST" => "Pastries",
+    ],
+    "MEMBERS" => [
+        "@TRANSLATION" => "Members",
+        "LIST" => "Members",
+    ]
+];
---
+++ app/sprinkles/extend-menu/locale/es_ES/menu.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    "PASTRIES" => [
+        "@TRANSLATION" => "Postres",
+        "LIST" => "Postres",
+    ],
+    "MEMBERS" => [
+        "@TRANSLATION" => "Miembros",
+        "LIST" => "Miembros",
+    ]
+];
